62c0365d2d154a464b80d522fa2448c312589f4e
Jessica Leach <jessicamaryleach@gmail.com>
2016-10-21 17:45:58 +0200

    books and films added in separate json file

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ var app = express();
 var fs = require('fs');
 
 
-var dataInMemory = JSON.parse(fs.readFileSync("filmdata.json").toString())["films"];
+var dataFilmInMemory = JSON.parse(fs.readFileSync("data.json").toString())["films"];
 
+var dataBookInMemory = JSON.parse(fs.readFileSync("data.json").toString())["books"];
 
 // var fs = require('fs');
 // var dataInMemory;
@@ -17,30 +18,50 @@ var dataInMemory = JSON.parse(fs.readFileSync("filmdata.json").toString())["film
 
 
 function findFilm(slug) {
-	for (var i = 0; i < dataInMemory.length; i++) {
-		if (dataInMemory[i].slug === slug) {
-			return dataInMemory[i];
+	for (var i = 0; i < dataFilmInMemory.length; i++) {
+		if (dataFilmInMemory[i].slug === slug) {
+			return dataFilmInMemory[i];
 		}
 	}
 }
 
+function findBook(slug) {
+	for (var i = 0; i < dataBookInMemory.length; i++) {
+		if (dataBookInMemory[i].slug === slug) {
+			return dataBookInMemory[i];
+		}
+	}
+}
 app.use(express.static('public'));
 
 app.get('/', function(request, response) {
-	response.redirect('/films');
+	response.redirect('/media');
+});
+
+app.get('/media', function(req, res) {
+	console.log('Requesting /media');
+	res.send(pug.renderFile('views/index.pug', { films: dataFilmInMemory, books: dataBookInMemory }));
+});
+
+app.get('/books', function(req, res) {
+	console.log('Requesting /books');
+	res.send(pug.renderFile('views/books.pug', { films: dataFilmInMemory }));
 });
 
 app.get('/films', function(req, res) {
 	console.log('Requesting /films');
-	res.send(pug.renderFile('views/index.pug', { films: dataInMemory }));
+	res.send(pug.renderFile('views/films.pug', { books: dataBookInMemory }));
 });
-
 
 app.get('/films/*', function(req, res) {
 	var foundFilm = findFilm(req.params[0]);
 	res.send(pug.renderFile('views/film.pug', { film: foundFilm }));
 });
 
+app.get('/books/*', function(req, res) {
+	var foundBook = findBook(req.params[0]);
+	res.send(pug.renderFile('views/book.pug', { book: foundBook }));
+});
 
 app.listen(3001, function() {
  console.log('Web server started on port 3001');
